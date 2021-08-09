002a91db243fbcaa1d793d62996e2979e5467683
Aayush Deep Ambasta <aayushdeepambasta@gmail.com>
2021-08-09 18:38:43 +0530

    asdf

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assests/pepperoni.jpg";
+import Margherita from "../assests/margherita.jpg";
+import PedroTechSpecial from "../assests/pedrotechspecial.jpg";
+import Vegan from "../assests/vegan.jpg";
+import Pineapple from "../assests/pineapple.jpg";
+import Expensive from "../assests/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 15.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
